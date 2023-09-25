bd3e45045a99065cb497f4b326ab5e4097386a1d
Chuck LU <chucklu2015@outlook.com>
2023-09-25 18:42:27 +0800

    unretweet

--- undoRepostMyTweets.js
+++ undoRepostMyTweets.js
@@ -1,22 +1,31 @@
-function nextUnlike() {
+function nextUnretweet() {
     return document.querySelector('[data-testid="unretweet"]')
-  }
-  
-  function wait(ms) {
+}
+
+function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
-  }
-  
-  async function removeAll() {
+}
+
+function confirmUnreweet(){
+    return document.querySelector('[data-testid="unretweetConfirm"]')
+}
+
+async function removeAll() {
     let count = 0
-    let next = nextUnlike()
+    let next = nextUnretweet()
     while (next) {
-      next.focus()
-      next.click()
-      console.log(`Unretweet ${++count} tweets`)
-      await wait(count % 50 === 0 ? 30000 : 2000)
-      next = nextUnlike()
+        next.focus()
+        next.click()
+
+        confirm=confirmUnreweet();
+        confirm.focus();
+        confirm.click();
+
+        console.log(`Unretweet ${++count} tweets`)
+        await wait(count % 50 === 0 ? 30000 : 2000)
+        next = nextUnretweet()
     }
     console.log('Out of unretweets, count =', count)
-  }
-  
-  removeAll() 
+}
+
+removeAll() 
