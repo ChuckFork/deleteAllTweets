7a4f0537fd6c970005f2932b013dfb216eb2bc94
Chuck LU <chucklu2015@outlook.com>
2023-09-26 10:12:31 +0800

    refresh the page when no unlike found

--- unlikeMyTweets.js
+++ unlikeMyTweets.js
@@ -9,13 +9,21 @@ function wait(ms) {
 
 async function removeAll() {
   let count = 0
+  let reloadCount=0;
   let next = nextUnlike()
-  while (next) {
+  while (true) {
     next.focus()
     next.click()
     console.log(`Unliked ${++count} tweets`)
     await wait(count % 50 === 0 ? 30000 : 2000)
     next = nextUnlike()
+    if(next===null){
+      location.reload();
+      reloadCount++;
+      if(reloadCount>=11){
+        break;
+      }
+    }
   }
   console.log('Out of unlikes, count =', count)
 }
